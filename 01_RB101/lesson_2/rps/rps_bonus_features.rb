51eda41c0074fdfc327ecc8a62a7c21ac74a9e00
Gwen Hoang <gwenkhoang@gmail.com>
2020-08-23 21:07:17 -0600

    add final rps bonus feature for code review

--- 01_RB101/lesson_2/rps/rps_bonus_features.rb
+++ 01_RB101/lesson_2/rps/rps_bonus_features.rb
@@ -1,99 +1,186 @@
-MAX_ROUNDS = 5
+=begin
+RPS Bonus Features
+Gwen Hoang
+v1 2020.08.23
+=end
+
+# --constants and dependencies--------------------------------------------------
+require 'yaml'
+MESSAGES = YAML.load_file('rps_bonus_messages.yml')
+WIN_SCORE = 5
+VALID_REPLAY = %w(y yes n no)
 
 VALID_CHOICES = {
-  's' => 'scissors', 
-  'p' => 'paper', 
-  'r' => 'rock', 
-  'l' => 'lizzard',
-  'o' => 'spock'}
-
-KEY_BEAT_VALUE = {
-  'scissors' => %w(paper lizzard),
-  'paper' => %w(rock spock),
-  'rock' => %w(lizzard scissors),
-  'lizzard' => %w(paper, spock),
-  'spock' => %w(rock scissors)
+  'r' => 'Rock',
+  'p' => 'Paper',
+  's' => 'Scissors',
+  'l' => 'Lizard',
+  'k' => 'Spock'
 }
 
-def prompt(message)
-  Kernel.puts("=> #{message}")
+WIN_RELATIONS = {
+  'Rock' => %w(Lizard Scissors),
+  'Paper' => %w(Rock Spock),
+  'Scissors' => %w(Paper Lizard),
+  'Lizard' => %w(Paper Spock),
+  'Spock' => %w(Rock Scissors)
+}
+
+# --methods---------------------------------------------------------------------
+def clear_screen
+  Kernel.system('clear') || Kernel.system('cls')
 end
 
-def get_user_input(message)
-  prompt(message)
-  Kernel.gets().chomp()
+def pace_interaction
+  Kernel.sleep(1)
+end
+
+def prompt(key, sub_data='')
+  message = MESSAGES[key]
+  if sub_data
+    message_with_sub = format(message, sub: sub_data)
+    puts("=> #{message_with_sub}")
+  else
+    puts("=> #{message}")
+  end
+end
+
+def display_instructions
+  prompt('instructions')
+  puts('')
+end
+
+def display_round(round)
+  prompt('round', round)
+end
+
+def get_user_input(message_key, invalid_message_key)
+  prompt(message_key)
+  input = ''
+  loop do
+    input = Kernel.gets().chomp()
+    break if valid_user_input?(message_key, input)
+    prompt(invalid_message_key)
+  end
+  input
+end
+
+def valid_user_input?(message_key, input)
+  case message_key
+  when 'game_start' then valid_game_start?(input)
+  when 'choice_letter' then valid_user_choice?(input)
+  when 'replay_choice' then valid_replay_choice?(input)
+  end
+end
+
+def valid_game_start?(choice)
+  choice.empty?()
+end
+
+def valid_user_choice?(choice_letter)
+  VALID_CHOICES.keys().include?(choice_letter.downcase)
+end
+
+def valid_replay_choice?(choice)
+  VALID_REPLAY.include?(choice.downcase)
 end
 
 def get_computer_choice
-  VALID_CHOICES[VALID_CHOICES.keys().sample()]
+  VALID_CHOICES.values().sample()
 end
 
-def valid_input?(letter)
-  VALID_CHOICES.keys().include?(letter)
+def display_choices(user_choice, computer_choice)
+  prompt('user_choice', user_choice)
+  prompt('computer_choice', computer_choice)
 end
 
-def win?(winner, loser)
-  KEY_BEAT_VALUE[winner].include?(loser)
-end
-
-def display_results(user, computer)
-  if win?(user, computer)
-    "You won!"
-  elsif win?(computer, user)
-    "Computer won!"
+def display_winner(user_choice, computer_choice)
+  if win?(user_choice, computer_choice)
+    prompt('user_win')
+  elsif win?(computer_choice, user_choice)
+    prompt('computer_win')
   else
-    "It's a tie!"
+    prompt('tie')
   end
 end
 
-def user_score(user, computer)
-  win?(user, computer) ? 1 : 0
+def win?(first, second)
+  WIN_RELATIONS[first].include?(second)
 end
 
-def computer_score(user, computer)
-  win?(computer,user) ? 1 : 0
+def calculate_score(total_score, user_choice, computer_choice)
+  total_score[:user] += 1 if win?(user_choice, computer_choice)
+  total_score[:computer] += 1 if win?(computer_choice, user_choice)
 end
 
+def display_total_score(round, total_score)
+  puts('')
+  prompt('total_score', round)
+  prompt('user_total_score', total_score[:user])
+  prompt('computer_total_score', total_score[:computer])
+  puts('')
+end
+
+def display_grand_winner(total_score)
+  prompt('user_grand_winner') if total_score[:user] == WIN_SCORE
+  prompt('computer_grand_winner') if total_score[:computer] == WIN_SCORE
+end
+
+def grand_winner?(total_score)
+  total_score[:user] == WIN_SCORE || total_score[:computer] == WIN_SCORE
+end
+
+def replay?(choice)
+  VALID_REPLAY[0..1].include?(choice.downcase)
+end
+
+def display_goodbye
+  prompt('goodbye')
+end
+
+# --main program----------------------------------------------------------------
+clear_screen
+display_instructions
+get_user_input('game_start', 'invalid_game_start')
+clear_screen
+
+total_score = { user: 0, computer: 0 }
 round = 1
-score_board = {
-  :user => 0,
-  :computer => 0
-}
-
-prompt("test")
 loop do
-  prompt("ROUND #{round}")
-
-  user_input = ""
-  user_choice = ""
   loop do
-    user_input = get_user_input("Enter a letter: ")
-    if valid_input?(user_input)
-      user_choice = VALID_CHOICES[user_input]
-      break
-    else
-      prompt("Not a valid choice!")
-    end
+    display_round(round)
+
+    choice_letter = get_user_input('choice_letter', 'invalid_choice_letter')
+    user_choice = VALID_CHOICES[choice_letter.downcase]
+    computer_choice = get_computer_choice
+
+    pace_interaction
+    display_choices(user_choice, computer_choice)
+    pace_interaction
+    display_winner(user_choice, computer_choice)
+    pace_interaction
+    calculate_score(total_score, user_choice, computer_choice)
+    display_total_score(round, total_score)
+    pace_interaction
+    clear_screen
+    break if grand_winner?(total_score)
+    round += 1
   end
 
-  p computer_choice = get_computer_choice
+  display_grand_winner(total_score)
+  display_total_score(round, total_score)
+  pace_interaction
 
-  result = display_results(user_choice, computer_choice)
+  replay_choice = get_user_input('replay_choice', 'invalid_replay_choice')
 
-  score_board[:user] += user_score(user_choice, computer_choice)
-  score_board[:computer] += computer_score(user_choice, computer_choice)
-
-  prompt("Your score is #{score_board[:user]}, Computer score is #{score_board[:computer]}")
-  
-  round += 1
-
-  if round < MAX_ROUNDS
-    prompt("Press any key for next round.")
-    pause = Kernel.gets().chomp()
-    system("clear")
+  if replay?(replay_choice)
+    round = 1
+    total_score[:user] = 0
+    total_score[:computer] = 0
+    clear_screen
   else
-    prompt("The match is over!")
+    break
   end
-
-  break if round > MAX_ROUNDS
 end
+
+display_goodbye
