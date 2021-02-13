9baa0017913969c965751e11eff7a5c6715cf042
Gwen Hoang <gwenkhoang@gmail.com>
2021-02-13 00:03:02 -0700

    final version of tic tac toe bonus features for code review

--- 01_RB101/lesson_6/tictactoe_bonus_features.rb
+++ 01_RB101/lesson_6/tictactoe_bonus_features.rb
@@ -1,3 +1,10 @@
+=begin
+Tic Tac Toe Bonus Features
+Gwen Hoang
+v0 2021.02.12
+=end
+
+# --constants and dependencies--------------------------------------------------
 require 'yaml'
 MESSAGES = YAML.load_file('tictactoe_bonus_messages.yml')
 
@@ -5,14 +12,19 @@ INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 DANGER_COMBO = 2
-GRAND_WINNER_SCORE = 2
+GRAND_WINNER_SCORE = 5
 PLAYER = 'Player'
 COMPUTER = 'Computer'
-VALID_INPUTS = [PLAYER, COMPUTER, 'Choose']
+VALID_TURN_INPUTS = [PLAYER, COMPUTER, 'Choose']
 VALID_REPLAY = %w(y yes n no)
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
                 [[1, 5, 9], [3, 5, 7]]
+
+# --methods---------------------------------------------------------------------
+def clear_screen
+  system('clear') || system('cls')
+end
 
 def prompt(key, sub_data='')
   msg = MESSAGES[key]
@@ -24,7 +36,13 @@ def prompt(key, sub_data='')
   end
 end
 
-def get_player_input(msg_key, sub_data = '', invalid_msg_key)
+def display_instructions
+  clear_screen
+  prompt('instructions')
+  get_player_input('start_round', 'invalid_start_round')
+end
+
+def get_player_input(msg_key, invalid_msg_key, sub_data = '')
   prompt(msg_key, sub_data)
   input = ''
   loop do
@@ -37,22 +55,22 @@ end
 
 def valid_player_input?(msg_key, input)
   case msg_key
-  when 'start' then valid_start?(input)
-  when 'square_choice' then valid_square?(input)
+  when 'first_turn' then valid_first_turn?(input)
+  when 'start_round' then valid_start_round?(input)
   when 'replay_choice' then valid_replay_choice?(input)
   end
 end
 
-def valid_start?(input)
+def valid_first_turn?(input)
+  VALID_TURN_INPUTS.include?(input.capitalize)
+end
+
+def valid_start_round?(input)
   input.empty?
 end
 
-def valid_square?(input)
-  empty_squares(brd).include?(input)
-end
-
 def valid_replay_choice?(input)
-  VALID_REPLAY.include?(choice.downcase)
+  VALID_REPLAY.include?(input.downcase)
 end
 
 def joinor(arr, delimiter1=', ', delimiter2='or')
@@ -63,10 +81,10 @@ end
 
 # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
 def display_board(brd, round_number)
-  # system('clear') || system('cls')
-  puts "Round #{round_number}"
-  puts "You are '#{PLAYER_MARKER}'"
-  puts "Computer is '#{COMPUTER_MARKER}'"
+  clear_screen
+  prompt('round', round_number)
+  prompt('player_marker', PLAYER_MARKER)
+  prompt('computer_marker', COMPUTER_MARKER)
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -93,54 +111,52 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def assign_first_turn
+  first_turn = get_player_input('first_turn', 'invalid_first_turn').capitalize
+  if first_turn == VALID_TURN_INPUTS[2]
+    first_turn = VALID_TURN_INPUTS[0..1].sample
+  end
+  prompt('first_move', first_turn)
+  first_turn
+end
+
 def player_move!(brd)
   options = joinor(empty_squares(brd))
-  square = get_player_input('square_choice', options, invalid_square_choice)
+  prompt('square_choice', options)
+  square = ''
+  loop do
+    square = gets.chomp.to_i
+    break if valid_square?(brd, square)
+    prompt('invalid_square_choice')
+  end
   brd[square] = PLAYER_MARKER
 end
 
+def valid_square?(brd, input)
+  empty_squares(brd).include?(input)
+end
+
 def computer_move!(brd)
-  square = nil
-  WINNING_LINES.each do |line|
-    WINNING_LINES.each do |line|
-      square = find_danger_square(line, brd, COMPUTER_MARKER)
-      break if square
-    end
-  end
-
-  if !square
-    WINNING_LINES.each do |line|
-    square = find_danger_square(line, brd, PLAYER_MARKER)
-    break if square
-    end
-  end
-
-  if !square && brd[5] == INITIAL_MARKER
-    square = 5
-  end
-
-  if !square
-    square = empty_squares(brd).sample
-  end
-
+  square = find_danger_square(brd, COMPUTER_MARKER)
+  square = find_danger_square(brd, PLAYER_MARKER) if !square
+  square == 5 if !square && brd[5] == INITIAL_MARKER
+  square = empty_squares(brd).sample if !square
   brd[square] = COMPUTER_MARKER
 end
 
 def find_danger_square(brd, marker_type)
   WINNING_LINES.each do |line|
     if brd.values_at(*line).count(marker_type) == DANGER_COMBO
-      brd.select do |marker_position, marker|
+      return (brd.select do |marker_position, marker|
         line.include?(marker_position) && marker == INITIAL_MARKER
-      end.keys.first
-    else
-      nil
+      end.keys.first)
     end
   end
+  nil
 end
 
 def place_piece!(brd, turn)
   turn == PLAYER ? player_move!(brd) : computer_move!(brd)
-
 end
 
 def alternate_player(turn)
@@ -158,12 +174,20 @@ end
 def detect_winner(brd)
   WINNING_LINES.each do |line|
     if brd.values_at(*line).all?(PLAYER_MARKER)
-      return PLAYER_WIN
+      return PLAYER
     elsif brd.values_at(*line).all?(COMPUTER_MARKER)
-      return COMPUTER_WIN
+      return COMPUTER
     end
   end
   nil
+end
+
+def display_round_result(brd, winner)
+  if someone_won?(brd)
+    prompt('someone_win', winner)
+  else
+    prompt('tie')
+  end
 end
 
 def calculate_score(total_points, winning_turn)
@@ -175,75 +199,57 @@ def calculate_score(total_points, winning_turn)
 end
 
 def display_score(player_points, computer_points)
-  prompt "Your score is #{player_points}"
-  prompt "Computer score is #{computer_points}"
+  prompt('player_score', player_points)
+  prompt('computer_score', computer_points)
 end
 
 def display_grand_winner(total_points)
   grand_winner = total_points.key(GRAND_WINNER_SCORE)
-  prompt "Grand Winner is #{grand_winner}" if grand_winner?(total_points)
+  prompt('grand_winner', grand_winner) if grand_winner?(total_points)
 end
 
 def grand_winner?(total_points)
   total_points.values.any? { |score| score == GRAND_WINNER_SCORE }
 end
 
+def replay?(choice)
+  VALID_REPLAY[0..1].include?(choice.downcase)
+end
+
+def display_goodbye
+  prompt('goodbye')
+end
+
+# --main program----------------------------------------------------------------
 loop do
+  display_instructions
   total_score = { You: 0, Computer: 0 }
   round = 1
-
-  current_player = ''
-
-  puts "Pick who to play"
-    start_choice = gets.chomp.downcase
-    if start_choice == 'choose'
-      # binding.pry
-      current_player = VALID_START_CHOICE[0..1].sample
-      prompt "#{current_player} goest first!"
-    else
-      current_player = start_choice
-    end
+  first_move = assign_first_turn
+  get_player_input('start_round', 'invalid_start_round')
   loop do
     board = initialize_board
-
+    current_player = first_move
     loop do
-      
-
-
       display_board(board, round)
       place_piece!(board, current_player)
       current_player = alternate_player(current_player)
-
       break if someone_won?(board) || board_full?(board)
-
     end
 
     display_board(board, round)
-
     winner = detect_winner(board)
-    if someone_won?(board)
-      prompt "#{winner} won!"
-    else
-      prompt "It is a tie!"
-    end
-
+    display_round_result(board, winner)
     player_score, computer_score = calculate_score(total_score, winner)
     display_score(player_score, computer_score)
-    
-    loop do
-      prompt "When you are ready, press the 'enter' key to continue."
-      choice = gets.chomp
-      break if choice.empty?()
-      prompt "Sorry, that is not a valid choice."
-    end
+    get_player_input('start_round', 'invalid_start_round')
     break if grand_winner?(total_score)
     round += 1
   end
 
   display_grand_winner(total_score)
-  prompt "Play again? (y or n)"
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  replay_choice = get_player_input('replay_choice', 'invalid_replay_choice')
+  break unless replay?(replay_choice)
 end
 
-prompt "Thanks for playing Tic Tac Toe! Good bye!"
+display_goodbye
